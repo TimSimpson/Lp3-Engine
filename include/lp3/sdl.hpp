93cb355a9786e47f3eda4f29ad3d086682cd8b99
Tim Simpson <timsimpson4@gmail.com>
2017-05-24 03:08:19 -0500

    Wraps SDL's RWops stuff

--- include/lp3/sdl.hpp
+++ include/lp3/sdl.hpp
@@ -80,6 +80,8 @@ using Texture = SdlAutoDeletedResource<SDL_Texture *, SDL_DestroyTexture>;
 using GLContext = SdlAutoDeletedResource<SDL_GLContext, SDL_GL_DeleteContext>;
 
 
+
+
 // Use these two classes to make the SDL_assert calls throw exceptions
 // instead to test that code properly triggers them.
 // NOTE: Currently this breaks the default behavior of SDL_assert afterwards
@@ -100,6 +102,66 @@ public:
 
 private:
     SDL_AssertionHandler old_handler;
+};
+
+LP3_CORE_API
+inline void close_rwops(SDL_RWops * ops) {
+	SDL_assert(nullptr != ops);
+	auto result = ops->close(ops);
+	SDL_assert(0 == result);
+}
+
+LP3_CORE_API
+class RWops {
+public:
+	RWops(gsl::owner<SDL_RWops *> ops);
+	
+	~RWops();
+
+	RWops(RWops && rhs);
+
+	RWops & operator=(RWops && rvalue);
+
+	// No copy ctor
+	RWops & operator=(const RWops & other) = delete;
+
+	// Implicitly convert to SDL_RWops
+	inline operator SDL_RWops * () {
+		SDL_assert(nullptr != ops);
+		return ops;
+	}
+
+	inline std::size_t read(void * dst, std::size_t object_count, 
+		                    std::size_t object_size = 1) {
+		SDL_assert(nullptr != ops);
+		return ops->read(ops, dst, object_count, object_size);
+	}
+	
+	inline std::int64_t seek(std::int64_t offset, int whence=RW_SEEK_CUR) {
+		SDL_assert(nullptr != ops);
+		return ops->seek(ops, offset, whence);
+	}
+
+	inline std::int64_t seek_from_beginning(std::int64_t offset) {
+		return seek(offset, RW_SEEK_SET);
+	}
+
+	inline std::int64_t seek_from_end(std::int64_t offset) {
+		return seek(offset, RW_SEEK_END);
+	}
+
+	inline std::int64_t size() {
+		SDL_assert(nullptr != ops);
+		return ops->size(ops);
+	}
+
+	inline std::size_t write(const void * src, std::size_t object_count,
+							 std::size_t object_size = 1) {
+		SDL_assert(nullptr != ops);
+		return ops->write(ops, src, object_count, object_size);
+	}
+private:
+	gsl::owner<SDL_RWops *> ops;
 };
 
 } }
