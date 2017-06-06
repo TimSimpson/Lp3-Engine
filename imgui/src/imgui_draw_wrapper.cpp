3871c68510a44e5914e9490f7f9e1c8f3d7839e8
Tim Simpson <timsimpson4@gmail.com>
2017-06-05 21:57:07 -0500

    Attempt two to fix the MSVC warnings

--- imgui/src/imgui_draw_wrapper.cpp
+++ imgui/src/imgui_draw_wrapper.cpp
@@ -1,8 +1,13 @@
 // There's probably an easier way to ignore warnings in CMake than this.
+#ifndef _MSC_VER
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunknown-pragmas"
 #pragma GCC diagnostic ignored "-Wuninitialized"
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
-#include "../lib/imgui_draw.cpp"
-#pragma GCC diagnostic pop
+#endif
 
+#include "../lib/imgui_draw.cpp"
+
+#ifndef _MSC_VER
+#pragma GCC diagnostic pop
+#endif
