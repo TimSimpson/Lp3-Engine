8cf1c6e9f535f7ba0d43d6c9ccf3270a54015402
Tim Simpson <timsimpson4@gmail.com>
2017-11-22 23:35:26 -0600

    Attempts to make this work in CI again

--- ci.py
+++ ci.py
@@ -32,17 +32,6 @@ DEPS_DIR = from_root('build/deps')
 SRC_DIR = from_root("""standalone""")
 
 
-def github_check_call(*args, **kwargs):
-    # Github can fail requests due to rate limiting.
-    for attempts in range(10):
-        try:
-            return subprocess.check_call(*args, **kwargs)
-        except:
-            print('Github call failed, retrying...')
-            time.sleep(1)
-    raise RuntimeError('Failed to call GitHub!')
-
-
 @cmd('clean', 'Wipes out build directory.')
 def clean(args):
     mkdir(BUILD_DIR)
@@ -59,80 +48,7 @@ def deps(args):
     if len(args) > 1:
         platform = args[0]
 
-    print('Checking out GSL...')
-    gsl_dir = os.path.join(DEPS_DIR, 'gsl')
-    mkdir(gsl_dir)
-    github_check_call([
-        'git', 'clone',
-        'https://github.com/Microsoft/GSL.git',
-        gsl_dir
-    ])
-    subprocess.check_call([
-        'git', 'checkout',
-        'c2f953f2eb7ab501325a7ec5656b400d54b8a345'
-    ], cwd=gsl_dir)
-    
-    
     print('Assuming Boost is already available.')
-    
-    
-    if not platform or platform == 'windows':
-        sdl2_url = 'https://9988350550b21bab76e1-e4f58d473b8b67b7df073d18b2ddc43c.ssl.cf1.rackcdn.com/sdl2-win-cmake.zip'
-        subprocess.check_call(['curl', '-o', 'sdl2.zip', sdl2_url], cwd=BUILD_DIR)
-    
-        name = 'sdl2'
-        url = sdl2_url
-        zip_name = '{}.zip'.format(name)
-        print('Downloading {}...'.format(zip_name))
-        subprocess.check_call(['curl', '-o', zip_name, url], cwd=BUILD_DIR)
-    
-        try:
-            ZipFile = zipfile.Zipfile  # py2
-        except AttributeError:
-            ZipFile = zipfile.ZipFile  # py3
-    
-        z = ZipFile(os.path.join(BUILD_DIR, zip_name))
-        z.extractall(os.path.join(DEPS_DIR, name))
-    
-    print('Downloading SDL2 Cmake Script from TwinklebearDev')
-    cmake_scripts = os.path.join(DEPS_DIR, 'cmake')
-    mkdir(cmake_scripts)
-    find_sdl_url = 'https://raw.githubusercontent.com/Twinklebear/TwinklebearDev-Lessons/master/cmake/FindSDL2.cmake'
-    github_check_call(['curl', '-o', 'FindSDL2.cmake', find_sdl_url],
-                      cwd=cmake_scripts)
-    
-    
-    
-    
-    
-    
-    
-    
-    print('Checking out Catch...')
-    catch_dir = os.path.join(DEPS_DIR, 'Catch')
-    mkdir(catch_dir)
-    github_check_call([
-        'git', 'clone',
-        'https://github.com/philsquared/Catch.git',
-        catch_dir
-    ])
-    subprocess.check_call([
-        'git', 'checkout', 'v1.8.2'
-    ], cwd=catch_dir)
-    
-    
-    print('Checking out GLM...')
-    glm_dir = os.path.join(DEPS_DIR, 'glm')
-    mkdir(glm_dir)
-    github_check_call([
-        'git', 'clone',
-        'https://github.com/g-truc/glm.git',
-        glm_dir
-    ])
-    subprocess.check_call([
-        'git', 'checkout',
-        '40398d67cd3e4f74b08649eda428dc411d801fd5'
-    ], cwd=glm_dir)
 
 
 @cmd('ubuntu', 'Build on Ubuntu')
@@ -181,19 +97,13 @@ def ubuntu(args):
         new_env['LP3_ROOT_PATH'] = os.path.join(SRC_DIR, 'media')
 
         options = []
-        options.append('-DCMAKE_BUILD_TYPE={}'.format(
+        options.append('--define CMAKE_BUILD_TYPE={}'.format(
             {'debug': 'Debug', 'release': 'Release'}[ap_args.type]))
         if ap_args.shared:
-            options.append('-DBUILD_SHARED_LIBS=true')
+            options.append('--define BUILD_SHARED_LIBS=true')
         if ap_args.clang_tidy:
             # TODO: add -warnings-as-errors with clang-tidy-4
-            options.append('-DCMAKE_CXX_CLANG_TIDY:STRING="clang-tidy-3.8;-checks=-*,readability-*,-std=c++14"')
-
-        # This is annoying. It exists only so I can use Bash for Ubuntu
-        # for Windows (aka Bfufw) as Git will try to check out a file with
-        # a colon there, screwing up the CMake call.
-        if ap_args.ntfs_work_around:
-            options.append('-DGSL_CHECKED_OUT=true')
+            options.append('--define CMAKE_CXX_CLANG_TIDY:STRING="clang-tidy-3.8;-checks=-*,readability-*,-std=c++14"')
 
         if ap_args.clean:
             print(' * spotless! *')
@@ -201,9 +111,27 @@ def ubuntu(args):
 
         mkdir(wb_dir)
 
+        with open('{}/requirements.txt'.format(SRC_DIR), 'r') as f:
+            reqs = f.read()
+        reqs = reqs.replace('\n', ' ')
+
         return (
             subprocess.call(
-                'cmake -G "Ninja" {options} -H{src_dir} -B{build_dir}'
+                'cget init {options} --prefix={wb_dir}/cget'.format(
+                    options=' '.join(options),
+                    wb_dir=wb_dir),
+                cwd=wb_dir,
+                shell=True,
+                )
+            or subprocess.call(
+                'cget install {}'.format(reqs),
+                shell=True,
+                cwd=wb_dir,
+                )
+            or subprocess.call(
+                'cmake -G "Ninja" '
+                '-DCMAKE_TOOLCHAIN_FILE={build_dir}/cget/cget/cget.cmake '
+                '-H{src_dir} -B{build_dir}'
                     .format(options=' '.join(options), src_dir=SRC_DIR,
                             build_dir=wb_dir),
                 shell=True,
@@ -274,6 +202,7 @@ def windows(args):
         mkdir(wb_dir)
 
         bit_str = {64: ' Win64', 32: ''}[ap_args.bits]
+        arch = {64: 'x64', 32: 'x86'}[ap_args.bits]
 
         # The following environment variables are needed by the tests.
         new_env = os.environ.copy()
@@ -281,7 +210,25 @@ def windows(args):
         new_env['LP3_ROOT_PATH'] = os.path.join(SRC_DIR, 'media')
 
 
+        with open('{}/requirements.txt'.format(SRC_DIR), 'r') as f:
+            reqs = f.read()
+        reqs = reqs.replace('\n', ' ')
+
         return (
+            subprocess.call(
+                'cget init --prefix={wb_dir}/cget '
+                '--define CMAKE_GENERATOR_PLATFORM={arch}'.format(
+                    arch=arch,
+                    wb_dir=wb_dir),
+                cwd=wb_dir,
+                shell=True,
+                )
+            or subprocess.call(
+                'cget install --{type} {reqs}'.format(
+                    type=ap_args.type, reqs=reqs),
+                shell=True,
+                cwd=wb_dir,
+                )
             subprocess.call(
                 'cmake -G "Visual Studio 14 2015{bits}" -H{src_dir} -B{build_dir}'
                     .format(src_dir=SRC_DIR, build_dir=wb_dir, bits=bit_str),
