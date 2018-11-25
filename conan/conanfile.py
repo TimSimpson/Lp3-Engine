bd32e90a2aaa3e67c975377a4f0439b95cfe2910
Tim Simpson <timsimpson4@gmail.com>
2018-11-25 10:56:37 -0600

    more conan stuff

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -3,20 +3,6 @@ import conans as c
 import os
 
 
-def _change(filename, find, replace):
-    new_lines = []
-    with open(filename, 'r') as f:
-        for line in f.readlines():
-            new_lines.append(line.replace(find, replace))
-
-    with open(filename, 'w') as f:
-        f.write(''.join(new_lines))
-
-
-def standardize_conan_packages():
-    _change("Findcatch2.cmake", "catch2::catch2", "Catch2::Catch2")
-
-
 class Lp3Engine(c.ConanFile):
 
     requires = (
@@ -28,11 +14,9 @@ class Lp3Engine(c.ConanFile):
         "catch2/2.4.1@bincrafters/stable"
     )
 
-    generators = "cmake_paths"  # "cmake_find_package"
+    generators = "cmake"
 
     def build(self):
-        # standardize_conan_packages()
-
         cmake = c.CMake(self)
-        cmake.configure(source_folder=os.path.join(self.source_folder, "core"))
+        cmake.configure()
         cmake.build()
