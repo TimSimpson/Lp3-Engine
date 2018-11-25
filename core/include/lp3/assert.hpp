393389732640b2ca425f568eaf7ab2b22ea6101f
Tim Simpson <timsimpson4@gmail.com>
2018-11-25 11:04:44 -0600

    go back to cget packages

--- core/include/lp3/assert.hpp
+++ core/include/lp3/assert.hpp
@@ -13,7 +13,7 @@
 #include "core/config.hpp"
 #include "core/Exception.hpp"
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
 // SDL_assert *almost* did what I wanted, but oops, it's crappy old C code
 // so there's no way to optionally raise an exception, which is what you
