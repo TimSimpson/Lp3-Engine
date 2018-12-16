5d2aa90a13785609fbeb70243953853fa6157295
Tim Simpson <timsimpson4@gmail.com>
2018-12-15 19:57:31 -0600

    fixes some clang errors

--- imgui/src/imgui_draw_wrapper.cpp
+++ imgui/src/imgui_draw_wrapper.cpp
@@ -1,13 +1,15 @@
 // There's probably an easier way to ignore warnings in CMake than this.
 #ifndef _MSC_VER
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunknown-pragmas"
-#pragma GCC diagnostic ignored "-Wuninitialized"
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wunknown-pragmas"
+    #pragma GCC diagnostic ignored "-Wuninitialized"
+    #ifndef __clang__
+        #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+    #endif
 #endif
 
 #include "../lib/imgui_draw.cpp"
 
 #ifndef _MSC_VER
-#pragma GCC diagnostic pop
+    #pragma GCC diagnostic pop
 #endif
