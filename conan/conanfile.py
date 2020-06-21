16022212f4f84ee8b1429534a28826087def5408
Tim Simpson <timsimpson4@gmail.com>
2020-06-21 00:38:26 -0500

    change lots trying to include bincrafters sdl mixer, but it won't build

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -14,15 +14,22 @@ class Lp3Engine(c.ConanFile):
     default_options = {"shared": False}
 
     requires = (
-
-        "gsl_microsoft/2.0.0@bincrafters/stable",
+        "Catch2/2.11.1@catchorg/stable",
         "glm/0.9.8.5@g-truc/stable",
+        "gsl_microsoft/2.0.0@bincrafters/stable",
         "sdl2/2.0.9@bincrafters/stable",
-        "zlib/1.2.11",
-        "libpng/1.6.37",
         "sdl2_image/2.0.5@bincrafters/stable",
         "sdl2_ttf/2.0.15@bincrafters/stable",
-        "Catch2/2.11.1@catchorg/stable"
+        "sdl2_mixer/2.0.4@bincrafters/stable",
+
+        # This stuff was brought in exclusively to resolve issues in
+        # transitive deps... yikes.
+        "bzip2/1.0.8",
+        "zlib/1.2.11",
+        "libpng/1.6.37",
+        "openssl/1.1.1g",
+        "glib/2.64.0@bincrafters/stable",
+        "libmodplug/0.8.9.0@bincrafters/stable",
     )
 
     generators = "cmake_find_package"
