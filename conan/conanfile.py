35986a2273e6a8bcda71d430d9be8af10390633e
Tim Simpson <timsimpson4@gmail.com>
2020-06-20 23:06:13 -0500

    updated `conan` directory. Holy cow that was easy

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -4,17 +4,28 @@ import os
 
 
 class Lp3Engine(c.ConanFile):
+    name = "Lp3-Engine"
+    version = "1.0.1"
+    author = "Tim Simpson"
+    url = "https://github.com/TimSimpson/Lp3-Engine"
+
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = {"shared": False}
 
     requires = (
+
         "gsl_microsoft/2.0.0@bincrafters/stable",
         "glm/0.9.8.5@g-truc/stable",
-        "sdl2/2.0.8@bincrafters/stable",
-        "sdl2_image/2.0.3@bincrafters/stable",
-        "sdl2_ttf/2.0.14@bincrafters/stable",
-        "catch2/2.4.1@bincrafters/stable"
+        "sdl2/2.0.9@bincrafters/stable",
+        "zlib/1.2.11",
+        "libpng/1.6.37",
+        "sdl2_image/2.0.5@bincrafters/stable",
+        "sdl2_ttf/2.0.15@bincrafters/stable",
+        "Catch2/2.11.1@catchorg/stable"
     )
 
-    generators = "cmake"
+    generators = "cmake_find_package"
 
     def build(self):
         cmake = c.CMake(self)
